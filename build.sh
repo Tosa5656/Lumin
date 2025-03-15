2fe03bc1d91b11d321fcb9772123bf93a2b539d8
Tosa <127298458+Tosa5656@users.noreply.github.com>
2025-03-15 13:37:46 +0400

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir bin obj
 gcc -m16 -ffreestanding -nostdlib -c src/kernel.c -o obj/kernel.o
 ld -m elf_i386 -T src/kernel-linker.ld -o bin/kernel.bin obj/kernel.o
 objcopy -O binary bin/kernel.bin bin/kernel.bin
