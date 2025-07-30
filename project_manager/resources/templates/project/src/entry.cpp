d85f10a9ee5de02361dcb1a8c3911d4b2a7888b5
Tosa <eea85670@gmail.com>
2025-07-30 11:33:28 +0400

    Project entry code

--- project_manager/resources/templates/project/src/entry.cpp
+++ project_manager/resources/templates/project/src/entry.cpp
@@ -1,4 +1,70 @@
+/*
+  _                            _              _____   _                 _   _         
+ | |                          (_)            / ____| | |               | | (_)        
+ | |       _   _   _ __ ___    _   _ __     | (___   | |_   _   _    __| |  _    ___  
+ | |      | | | | | '_  _ \  | | | '_ \     \___ \  | __| | | | |  / _ | | | |  / _ \ 
+ | |____  | |_| | | | | | | | | | | | | |    ____) | | |_  | |_| | | (_| | | | | (_) |
+ |______|  \__,_| |_| |_| |_| |_| |_| |_|   |_____/   \__|  \__,_|  \__,_| |_|  \___/ 
+                                                                                      
+*/
 #include <iostream>
 #include <Lumin.h>
+#include "Lumin/ScriptAPI/ScriptAPI.h"
 
-int main() {};
+GLFWwindow* g_window = nullptr;
+
+void Awake() {}
+
+void Start() {}
+
+void Update() 
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+
+    ImGui::NewFrame();
+    ImGui::End();
+
+    ObjectsManager::DrawObjects();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    Projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+}
+
+#ifdef _WIN32
+    #ifdef RELEASE_BUILD
+        #include <Windows.h>
+        int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+    #else
+        int main()
+    #endif
+#else
+    int main()
+#endif
+{
+    #ifdef RELEASE_BUILD
+    #else
+        std::cout << "Lumin started in debug mode." << std::endl;
+    #endif
+
+    Lumin::Windowing::Window window(800, 600, "Lumin", Awake, Start);
+    g_window = window.GetGLFWwindow();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(g_window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    glfwSetInputMode(g_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetFramebufferSizeCallback(g_window, framebuffer_size_callback);
+    window.run(Update);
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    return 0;
+}
